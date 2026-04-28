9c270aa4011340d0d0cc313c68d5c4d1268a2fa0
Douglas Heliston Ferreira Batista <douglas.batista@cassi.com.br>
2026-04-27 22:52:59 -0300

    fix: remove outputFileTracingRoot com path local (não funciona em produção)

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: '/Users/douglasheliston/.paperclip/instances/default/projects/8b0b06a9-24dd-4c7d-8eca-171b1ab9b1a8/bcd8d622-e282-450b-a5c8-7fc9e4a945e2/_default/casa-landing',
+
 }
 
 export default nextConfig
